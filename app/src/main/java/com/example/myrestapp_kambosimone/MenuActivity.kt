56303667ca0f4ddd00c7b23d8da218d0d78530d3
binf01-Kambo <simonekambo1@gmail.com>
2025-06-05 11:23:01 +0200

    Ho collegato l'activity del sito netflix

--- app/src/main/java/com/example/myrestapp_kambosimone/MenuActivity.kt
+++ app/src/main/java/com/example/myrestapp_kambosimone/MenuActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myrestapp_kambosimone
 
+import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebView
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +15,12 @@ class MenuActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
 
+        val btnWebView = findViewById<ImageButton>(R.id.btnWebView)
+        val btnCercaFilm = findViewById<ImageButton>(R.id.btnCercaFilm)
+
+        btnWebView.setOnClickListener {
+            val mioIntent = Intent(this, WebViewActivity::class.java)
+            startActivity(mioIntent)
+        }
     }
 }
