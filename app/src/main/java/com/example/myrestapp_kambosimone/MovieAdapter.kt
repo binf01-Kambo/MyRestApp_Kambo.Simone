adffa2912a0c2d090df592e808cdee735c1e5bd7
binf01-Kambo <simonekambo1@gmail.com>
2025-06-05 11:26:16 +0200

    importato progetto prof

--- app/src/main/java/com/example/myrestapp_kambosimone/MovieAdapter.kt
+++ app/src/main/java/com/example/myrestapp_kambosimone/MovieAdapter.kt
@@ -1,20 +1,37 @@
 package com.example.myrestapp_kambosimone
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class MovieAdapter : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_movie_adapter)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+class MovieAdapter(private val movies: List<Movie>) :
+    RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+
+    class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView = view.findViewById(R.id.tvTitolo)
+        val year: TextView = view.findViewById(R.id.tvAnno)
+        val poster: ImageView = view.findViewById(R.id.ivPoster)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_movie_adapter, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val movie = movies[position]
+        holder.title.text = movie.Title
+        holder.year.text = movie.Year
+        if (movie.Poster != "N/A") {
+            Glide.with(holder.itemView.context).load(movie.Poster).into(holder.poster)
+        } else {
+            holder.poster.setImageResource(R.drawable.ic_launcher_background)
         }
     }
+
+    override fun getItemCount(): Int = movies.size
 }
