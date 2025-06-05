e86c4f218b0956b877b11d93d89dcf6eb5eea392
binf01-Kambo <simonekambo1@gmail.com>
2025-06-05 11:08:35 +0200

    Ho creato la funzione di cifratura

--- app/src/main/java/com/example/myrestapp_kambosimone/LoginActivity.kt
+++ app/src/main/java/com/example/myrestapp_kambosimone/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.myrestapp_kambosimone
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import java.security.MessageDigest
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +13,18 @@ class LoginActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
+        val btnlogin = findViewById<Button>(R.id.btnLogin)
+
+        btnlogin.setOnClickListener {
+            val nomeUtente = findViewById<EditText>(R.id.nomeUtente).text.toString()
+            val password = findViewById<EditText>(R.id.password).text.toString()
+
+
+        }
+    }
+
+    private fun sha256(input: String): String {
+        val bytes = MessageDigest.getInstance("SHA-256").digest(input.toByteArray())
+        return bytes.joinToString("") { "%02x".format(it) }
     }
 }
